159c0bde9dbdd3a6dae9df324fa58df054486d52
Michael Kim <michael.kim.200518@gmail.com>
2021-05-27 15:27:22 +0900

    update

---
+++ README.md
@@ -0,0 +1,46 @@
+# concert-stoa-sample-data
+
+# 설치하기
+
+Stoa 프로젝트에서 임의 폴더를 하나 생성한다. (ex .npm)
+```sh
+$ mkdir .npm
+$ cd .npm
+$ git clone https://github.com/MichaelKim20/convert-stoa-sample-data.git
+$ cd convert-stoa-sample-data
+```
+
+# 이전 블럭의 UTXO의 참조테이블 생성하기
+
+이작업은 변경되기전의 boa-sdk-ts를 사용하여야 한다.
+
+```sh
+ts-node make-utxo-lookup ./old/sample.json
+```
+출력된 결과를 ./old/sample_utxo.json 에 저장한다.
+
+
+```sh
+ts-node make-utxo-lookup ./old/recovery.json
+```
+출력된 결과를 ./old/recovery_utxo.json 에 저장한다.
+
+
+# 최신 버전의 boa-sdk-ts로 업그레이드하기
+
+
+# 새로운 블록데이터 생성하기
+
+블록 구조의 변경범위에 따라서 convert-blocks.ts를 수정해야 될 경우가 있따.
+
+
+```sh
+ts-node convert-blocks ./old/sample.json ./old/sample_utxo.json
+```
+출력된 결과를 ./new/sample.json 에 저장한다.
+
+
+```sh
+ts-node convert-blocks ./old/recovery.json ./old/recovery_utxo.json
+```
+출력된 결과를 ./new/recovery.json 에 저장한다.
--- convert-blocks.ts
+++ convert-blocks.ts
@@ -4,23 +4,32 @@ import {
     Hash,
     hashFull,
     hashMulti,
+    KeyPair,
+    Lock,
+    LockType,
     makeUTXOKey,
-    PublicKey, KeyPair,
+    Pair,
+    PublicKey,
+    Scalar,
+    Schnorr,
     Signature,
     SodiumHelper,
     Transaction,
     TxInput,
-    TxOutput,
-    JSBI,
-    Unlock
+    TxOutput, TxType,
+    Unlock,
+    JSBI
 } from 'boa-sdk-ts';
 
-import { WK } from './WK';
+import {WK} from './WK';
 
 import * as fs from 'fs';
 
 import { old_key } from "./old_key";
 import { new_key } from "./new_key";
+import {BOASodium} from "boa-sodium-ts";
+import {toString} from "ip";
+import {add} from "winston";
 
 let key_map: Map<string, string> = new Map<string, string>();
 for (let idx = 0; idx < old_key.length && idx < new_key.length; idx++)
@@ -41,6 +50,7 @@ if (!Array.isArray(old_blocks))
     console.log("블록파일은 배열형태여야 합니다.");
     process.abort();
 }
+
 if (!Array.isArray(utxo_lookup))
 {
     console.log("UTXO Look up 은 배열형태여야 합니다.");
@@ -66,6 +76,7 @@ let findByUTXO = (hash: string) => {
 let new_block_data: Array<any> = [];
 
 (async () => {
+    SodiumHelper.assign(new BOASodium());
     await SodiumHelper.init();
 
     // Replace Transaction
@@ -75,36 +86,29 @@ let new_block_data: Array<any> = [];
             let new_block = JSON.parse(JSON.stringify(block));
             let txs = new Array<Transaction>();
             let merkle_tree: Array<Hash> = [];
+            let tx_idx = 0;
             for (let tx of block.txs) {
                 let type = tx.type;
                 let inputs = new Array<TxInput>();
                 let outputs = new Array<TxOutput>();
                 for (let input of tx.inputs) {
                     let utxo = new Hash(input.utxo);
-                    let signature = new Signature(input.signature);
-                    inputs.push(new TxInput(utxo, Unlock.fromSignature(new Signature(Buffer.alloc(Signature.Width)))));
+                    inputs.push(new TxInput(utxo, Unlock.reviver("", input.unlock)));
                 }
 
+                let output_idx = 0;
                 for (let output of tx.outputs) {
                     let value = JSBI.BigInt(output.value);
-                    let new_address = key_map.get(output.address);
-                    let publicKey: PublicKey;
-                    if (new_address !== undefined)
-                    {
-                        output.address = new_address;
-                        publicKey = new PublicKey(new_address);
-                    }
-                    else
-                    {
-                        publicKey = new PublicKey("boa1xrdwryl0ajdd86c45w4zrjf8spmrt7u4l7s5jy64ac3dc78x2ucd7wkakac");
-                        console.error("Can not found new well known key!");
-                    }
-                    outputs.push(new TxOutput(value, publicKey));
+                    let lock = Lock.reviver("", output.lock);
+                    outputs.push(new TxOutput(value, lock));
+                    output_idx++;
                 }
-                let payload = new DataPayload(tx.payload);
+                let payload = new DataPayload(tx.payload.bytes);
                 let new_tx = new Transaction(type, inputs, outputs, payload);
+
                 txs.push(new_tx);
                 merkle_tree.push(hashFull(new_tx));
+                tx_idx++;
             }
             buildMerkleTree(merkle_tree);
             new_block.merkle_tree = merkle_tree.map(n => n.toString());
@@ -117,40 +121,99 @@ let new_block_data: Array<any> = [];
                 new_block.header.validators = "[252]";
                 new_block.header.random_seed = hashFull(block_idx).toString();
             }
+            delete new_block.header.timestamp;
+            new_block.header.time_offset = block_idx * (10*60);
             new_block_data.push(new_block);
             block_idx++;
-
-            //console.log(JSON.stringify(block));
         }
     }
 
+    //console.log(JSON.stringify(new_block_data));
     let blocks: Array<Block>;
+
     // Create Block Instance
     blocks = new_block_data.map(n => Block.reviver("", n));
 
     // Fix values
     //  Transaction Input
+    let block_index = 0;
     for (let block of blocks) {
         if (JSBI.lessThan(block.header.height.value, JSBI.BigInt(1)))
             continue;
 
+        let tx_index = 0;
         for (let tx of block.txs) {
+            let in_index = 0;
             for (let tx_input of tx.inputs) {
                 let find_value = findByUTXO(tx_input.utxo.toString());
                 if (find_value === undefined) {
-                    console.error("Can not found UTXO!");
+                    console.error("Can not found");
                 }
                 else {
                     let tx2 = blocks[find_value.block_idx].txs[find_value.tx_idx];
-                    tx_input.utxo = makeUTXOKey(hashFull(tx2), JSBI.BigInt(find_value.out_idx));
 
-                    let address = new PublicKey(tx2.outputs[find_value.out_idx].lock.bytes);
-                    let keypair = WK.keys(address.toString());
-                    tx_input.unlock = Unlock.fromSignature(keypair.secret.sign<Transaction>(tx));
+                    let ox = -1;
+                    let i = 0;
+                    for (let o of tx2.outputs)
+                    {
+                        let address = (new PublicKey(o.lock.bytes)).toString();
+                        if (find_value.address === address)
+                        {
+                            ox = i;
+                            break;
+                        }
+                        i++;
+                    }
+
+                    if (ox == -1)
+                        console.error("Can not found utxo(address)");
+
+                    tx_input.utxo = makeUTXOKey(hashFull(tx2), JSBI.BigInt(ox));
+
+                    try {
+
+                    let pk = new PublicKey(tx2.outputs[find_value.out_idx].lock.bytes);
+                    let keypair = WK.keys(pk.toString());
+
+                    /*
+                    let scalar: Scalar = KeyPair.secretKeyToCurveScalar(keypair.secret)
+                    let pair: Pair = new Pair(scalar, scalar.toPoint());
+                    let sig = Schnorr.signPair<Transaction>(pair, tx);
+                    tx_input.unlock = Unlock.fromSignature(sig);
+                    */
+                    let tx_hash = hashFull(tx);
+                    let sig = keypair.secret.sign(tx_hash.data);
+                    tx_input.unlock = Unlock.fromSignature(sig);
+                    }
+                    catch (error)
+                    {
+                        console.log()
+                        console.log(`${block_index}:${tx_index}:${in_index}`)
+                    }
                 }
+                in_index++;
             }
+            tx_index++;
         }
+        block_index++;
     }
+    /*
+    let input_tx = blocks[1].txs[1];
+
+    let input_utxo = makeUTXOKey(hashFull(input_tx), BigInt(0));
+    let pk = new PublicKey("GDPF22L2ZO6SNZRT7NR6U5OVLT5YJZUQTS4Y3D75PIGBGFZAZU6QNLED");
+    let keypair = WK.keys(pk.toString());
+
+    let scalar: Scalar = KeyPair.secretKeyToCurveScalar(keypair.secret)
+    let pair: Pair = new Pair(scalar, scalar.toPoint());
+    let sig = Schnorr.signPair<Transaction>(pair, input_tx);
+
+    let input = new TxInput(input_utxo, new Unlock(Buffer.concat([sig.data, pk.data])));
+    let output = new TxOutput(BigInt(24400000000000), Lock.fromPublicKey(new PublicKey("GDPG22IINMKXTPYB3QQUGKUHQXBZKSN2XM4LACUANNJGRYYE6JTQFV6K")));
+    let new_tx = new Transaction(TxType.Payment, [input], [output], new DataPayload(Buffer.alloc(0)));
+
+    blocks[2].txs.push(new_tx);
+    */
 
     //  Enrollment
     for (let block of blocks) {
@@ -162,7 +225,23 @@ let new_block_data: Array<any> = [];
             }
             else {
                 let tx2 = blocks[find_value.block_idx].txs[find_value.tx_idx];
-                en.utxo_key = makeUTXOKey(hashFull(tx2), JSBI.BigInt(find_value.out_idx));
+
+                let ox = -1;
+                let i = 0;
+                for (let o of tx2.outputs)
+                {
+                    let address = (new PublicKey(o.lock.bytes)).toString();
+                    if (find_value.address === address)
+                    {
+                        ox = i;
+                        break;
+                    }
+                    i++;
+                }
+
+                if (ox == -1)
+                    console.error("Can not found utxo(address)");
+                en.utxo_key = makeUTXOKey(hashFull(tx2), JSBI.BigInt(ox));
             }
         }
     }
@@ -185,9 +264,9 @@ let new_block_data: Array<any> = [];
         block_hash = hashFull(block.header);
     }
 
-    let block_objs = JSON.parse(JSON.stringify(blocks));
-    for (let block of block_objs) {
-        block.header.validators = JSON.stringify(block.header.validators.storage);
-    }
-    console.log(JSON.stringify(block_objs));
+    //let block_objs = JSON.parse(JSON.stringify(blocks));
+    //for (let block of block_objs) {
+    //    block.header.validators = JSON.stringify(block.header.validators.storage);
+   // }
+    console.log(JSON.stringify(blocks));
 })();
