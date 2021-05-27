027bb7f25ca751b53814e486b46d303e0093bb9d
Michael Kim <michael.kim.200518@gmail.com>
2021-05-27 15:28:15 +0900

    Delete KeyConvert.ts

--- KeyConvert.ts
+++
@@ -1,9 +0,0 @@
-
-import { old_key } from "./old_key";
-import { new_key } from "./new_key";
-
-let keys: Map<string, string> = new Map<string, string>();
-for (let idx = 0; idx < old_key.length && idx < new_key.length; idx++)
-    keys.set(old_key[idx], new_key[idx]);
-
-console.log(keys);
